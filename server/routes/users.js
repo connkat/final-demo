7188074623fe0b58d32cf01bbc452d86d2ac8b29
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-08-04 11:52:56 -0400

    add user route with JSON response

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  res.json(['Vasiliy, Abigail, Aky, Ann, Boly, Calvin'])
 });
 
 module.exports = router;
